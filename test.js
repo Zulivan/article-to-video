aab9728589f0ecd6bff3b73b2b313e9dcf46f1e1
Zulivan <bill.doritos@laposte.net>
2019-06-10 11:19:55 +0200

    test

--- test.js
+++ test.js
@@ -1,14 +1,7 @@
-const txtomp3 = require('text-to-mp3');
+const img = require('../tools/ImageCreator.js');
 
-const sentence = 'how can we determine the problem'
+img
 
-const langcode = 'fr-ca'
-
-// en // BRIT
-// en-US // AMERICAN
-// en-AU // Australian
-// en-IN // Indian
-
-txtomp3.saveMP3(langcode, sentence, './vocal-'+langcode+'.mp3', function(err1, absoluteFilePath){
-	
+img.h2p('Success', 'French English (lul)').then((file) => {
+    console.log('On a gagné! Allez les blues allez...')
 });
